3ca89983b0f1a33ddaea9eb3444fa28c4ab9e4e9
Dan Smith <dan@callrail.com>
2021-02-26 16:37:59 -0500

    Wire up Honeybadger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import Honeybadger from '@honeybadger-io/js';
+
+Honeybadger.configure({
+  apiKey: process.env.HONEYBADGER_API_KEY || '',
+  environment: process.env.NODE_ENV || 'development',
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(Honeybadger.requestHandler);
   await app.listen(3000);
+  app.use(Honeybadger.errorHandler);
 }
 bootstrap();
